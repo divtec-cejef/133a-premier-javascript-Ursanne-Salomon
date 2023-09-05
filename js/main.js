bd2812d8763318103b319c73664bcc5f07d24796
salours <ursanne.salomon@divtec.ch>
2023-09-05 14:46:27 +0200

    Ajout d'un test

--- js/main.js
+++ js/main.js
@@ -5,6 +5,22 @@
  * @since 2023-08-29
  */
 "use strict";
-let msg = "Bonjour";
+// Récupèrer les paragraphes du document
+let tableau = document.querySelectorAll("p");
+let btAjouter = document.querySelector("button.Ajouter");
+let txtArticle = document.querySelector("#article");
+let ulArticle = document.querySelector("ul.article");
+// Affiche les contenu de la variable dans la console
+console.log(tableau,btAjouter);
 
-console.log(msg);
+// Ecoute le clique sur le bouton
+btAjouter.addEventListener("click", () =>
+    ulArticle.innerHTML += "<li>" + txtArticle.value + "</li>");
+
+let age = prompt("Quel est ton age ?");
+age = parseInt(age);
+if(isNaN(age)) {
+    alert("Entrer un nombre");
+} else {
+    alert(`tu a ${age} ans !`);
+}
